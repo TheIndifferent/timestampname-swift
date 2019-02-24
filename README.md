70e65581947937dac744b0f58ece288994ea1b08
Stanislav Baiduzhyi <baiduzhyi.devel@gmail.com>
2019-02-24 19:06:31 +0100

    Initial commit of Hello World app.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # timestampname-swift
+
 Small command line application to rename photo and video files to a timestamp format according to metadata tags
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import timestampname_swiftTests
+
+var tests = [XCTestCaseEntry]()
+tests += timestampname_swiftTests.allTests()
+XCTMain(tests)
